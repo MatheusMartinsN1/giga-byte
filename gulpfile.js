954538c888f4b26c43af4cac44ab2fbf5dd328e3
MatheusMartinsN1 <mateusdecampos34@icloud.com>
2025-06-12 22:25:38 -0300

    Carousel de imagens e shows finalizado

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ let gulp = require('gulp');
 let sass = require('gulp-sass')(require('sass'));
 let sourcemaps = require('gulp-sourcemaps');
 let imagemin = require('gulp-imagemin');
+let uglify = require('gulp-uglify');
 
 // Tarefa para desenvolvimento
 function stylesDev() {
@@ -23,19 +24,27 @@ function stylesBuild() {
         .pipe(gulp.dest('./build/styles'))
 }
 
-// Função do ImageMin
+// Tarefa de minificação de imagens
 function images() {
     return gulp.src('./src/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./build/images'))
 }
 
+// Tarefa de comprimir arquivo JS
+function comprimeJs() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/scripts'))
+}
+
 // Função do watch files
 function watchFiles() {
     gulp.watch('./src/styles/*.scss', { ignoreInitial: false}, stylesDev);
     gulp.watch('./src/images/*', { ignoreInitial: false}, images);
+    gulp.watch('./src/scripts/*.js', { ignoreInitial: false}, comprimeJs);
 }
 
-exports.dev = gulp.series(stylesDev, watchFiles, images);
-exports.build = gulp.series(stylesBuild, images);
+exports.dev = gulp.series(stylesDev, watchFiles, images, comprimeJs);
+exports.build = gulp.series(stylesBuild, images, comprimeJs);
 exports.default = exports.dev;
---
+++ src/scripts/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    // Função do carousel
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: false,
+    autoplayTimeout: 3000,
+    responsive: {
+        0: { items: 1 },
+        600: { items: 2 },
+        1000: { items: 3 }
+        }
+    })
+
+    // Função do menu
+    $('.btn-menu').click(function(){
+        $('.btn-menu').removeClass('active');
+        $(this).addClass('active');
+        $('.sabores__container__listagem').hide();
+        let target = $(this).data('target');
+        $(target).fadeIn();
+    })
+})
